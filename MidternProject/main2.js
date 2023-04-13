b2482ba76cc90b6362781d55703f769fb18434fa
qixiang <qixiang1009@gmail.com>
2023-04-14 06:15:26 +0800

    Fix bug.

--- MidternProject/main2.js
+++ MidternProject/main2.js
@@ -211,10 +211,12 @@ d3.csv("dataanime.csv", type).then((res) => {
 let preloader = document.querySelector("#preloader");
 let footer = document.querySelector(".footer");
 let controls = document.querySelector(".controls");
+let table_container = document.querySelector(".table-container");
 if (preloader) {
     window.addEventListener("load", () => {
         preloader.remove();
         footer.style.opacity = "1.0";
         controls.style.opacity = "1.0";
+        table_container.style.opacity = "1.0";
     });
 }
